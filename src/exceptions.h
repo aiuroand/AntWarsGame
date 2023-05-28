b550add97566ea35e6fcc81ee7f3653b59b6767f
Andrei Aiurov <aiuroand@fit.cvut.cz>
2023-05-28 16:27:28 +0200

    7.5

--- src/exceptions.h
+++ src/exceptions.h
@@ -1,7 +1,16 @@
 #ifndef EXCEPTIONS_H
 #define EXCEPTIONS_H
+#include <iostream>
 
 class CLeave{};
 class CSave{};
+class CFileReadingError
+{
+  public:
+    CFileReadingError ( std::string f )
+    : m_FileName ( f )
+    {}
+    std::string m_FileName;
+};
 
 #endif //EXCEPTIONS_H
